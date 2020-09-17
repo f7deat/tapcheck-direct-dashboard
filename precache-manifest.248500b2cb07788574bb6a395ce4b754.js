c09a33b59a3fdf9605bc8098101fd81f63b318fe
TanDC_VM <tandc_vm@novaon.dc>
2020-09-17 11:29:50 +0700

    Updates

---
+++ precache-manifest.248500b2cb07788574bb6a395ce4b754.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae079f5c826f147eaccc3e2e62fd72e8",
+    "url": "/tapcheck-direct-dashboard/index.html"
+  },
+  {
+    "revision": "4e3e5a1b7cc8bd919905",
+    "url": "/tapcheck-direct-dashboard/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "2215a29249732be36b34",
+    "url": "/tapcheck-direct-dashboard/static/css/main.1f5bcee1.chunk.css"
+  },
+  {
+    "revision": "4e3e5a1b7cc8bd919905",
+    "url": "/tapcheck-direct-dashboard/static/js/2.b12b1c5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tapcheck-direct-dashboard/static/js/2.b12b1c5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2215a29249732be36b34",
+    "url": "/tapcheck-direct-dashboard/static/js/main.1189229d.chunk.js"
+  },
+  {
+    "revision": "ac739dfa5ee99825cafb",
+    "url": "/tapcheck-direct-dashboard/static/js/runtime-main.b214a4e0.js"
+  },
+  {
+    "revision": "8846e0395a496323f483a3543888ea1b",
+    "url": "/tapcheck-direct-dashboard/static/media/apple_store.8846e039.svg"
+  },
+  {
+    "revision": "74b7d11f11ee375d709f42b493049c2b",
+    "url": "/tapcheck-direct-dashboard/static/media/google_play.74b7d11f.svg"
+  }
+]);
